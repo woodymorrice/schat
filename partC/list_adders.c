4d5608af293d5ccdeaf4a9f9ff82c22510b9f3eb
phong <wdz468@usask.ca>
2023-09-16 16:21:21 -0600

    Draft for list_adders.c

--- partC/list_adders.c
+++ partC/list_adders.c
@@ -7,28 +7,99 @@ const int MAX_ITEM = 10;
  
 LIST *ListCreate () {
     LIST *newList; 
-    newList->headPointer = NULL;
-    newList->tailPointer = NULL;
-    newList->currentItem = NULL;
+    newList->*headPointer = NULL;
+    newList->*tailPointer = NULL;
+    newList->*currentItem = NULL;
     newList->totalItem = 0;
     return newList;
 }
 
 int ListCount (LIST *curList) {
-    
+    return curList->totalItem;    
 }
 
 int ListAppend (LIST *curList, NODE *item) {
-    LIST *prevItem = newList->tailPointer; 
+    NODE *prevTail = *(curList->*tailPointer); 
     /*
     * If the current pointer is at the end of list,
     * item is added at the end.
     */
     if (curList->totalItem < MAX_ITEM) {
-
-        if (curList->tailPointer == curList->currentItem) {
-
-            curList->tailPointer = 
+        ListLast(curList);
+        curList->*tailPointer = item;
+        item->*prevNode = prevTail;
+        item->*nextNode = NULL;
+        /*
+        * Making sure the tail is new item and current item
+        * at the tail 
+        */
+        ListLast(curList);
+        curList->totalItem += 1;
+        return 0;
+    }
+    return -1;
 }
+
+int ListPrepend (LIST *curList, NODE *item) {
+    NODE *prevHead = *(curList->*headPointer);
+    /*
+    * If the current pointer is at the head of list,
+    * item is added at the end.
+    */
+    if (curList->totalItem < MAX_ITEM) {
+        ListFirst(curList);
+        curList->*headPointer = item;
+        item->*prevNode = NULL;
+        item->*nextNode = prevHead;
+        /*
+        * Making sure the head is new item and current item
+        * at the head
+        */
+        LisFirst(curList);
+        curList->totalItem += 1;
+        return 0;
+    }
+    return -1;
 }
+
+int ListAdd(LIST *curList, NODE *item) {
+    NODE *curItem = ListCurr(curList);
+    NODE *curNext = *(ListCurr(curList))->*nextNode;
+    if (curList->totalItem < MAX_ITEM) {
+        if (curList->*currentItem == curList->*tailPointer) {
+            /*
+            * if current item is at tail
+            */
+            return ListAppend(curList, item);
+        }
+        *(curList->*currentItem)->*nextNode = item; 
+        curNext->*prevNode = item;
+        item->*nextNode = curNext;
+        item->*prevNode = curItem;
+        ListNext(curList);
+        curList->totalItem += 1;
+        return 0;    
+    }
+    return -1;
+}
+
+int ListInsert(LIST *curList, NODE *item) {
+    NODE *curItem = ListCurr(curList);
+    NODE *curPrev = *(ListCurr(curList))->*prevNode;
+    if (curList->totalItem < MAX_ITEM) {
+        /*
+        * if current item is at head
+        */
+        if (curList->*currentItem == curList->*headPointer) {
+            return ListPrepend(curList, item);
+        }
+        *(curList->*currentItem)->*prevNode = item;
+        curPrev->*nextNode = item;
+        item->*prevNode = curPrev;
+        item->*nextNode = curItem;
+        ListPrev(curList);
+        curList->totalItem += 1;
+        return 0;
+    }
+    return -1;
 }
