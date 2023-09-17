45d8a04514333f957c73a53a814022b60ca77d5a
phong <wdz468@usask.ca>
2023-09-17 10:26:32 -0600

    Mostly done with initial errors for what was written

--- partC/mytestlist.c
+++ partC/mytestlist.c
@@ -3,3 +3,14 @@
 #include <list.h>
 
 
+
+int main(int argc, char* argv[]) {
+
+
+if (argc != 2) {
+    perror("Usage: perror. wrong number of arguments");
+    return -1;
+}
+
+int test = atoi(argv[1]);
+}
