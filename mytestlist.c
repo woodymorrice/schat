0b6e1a9f54c44789cdc2aa4f97bf7dbb0fb38191
phong <wdz468@usask.ca>
2023-09-17 11:47:35 -0600

    Copied files for testing

---
+++ mytestlist.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <list.h>
+
+
+
+int main(int argc, char* argv[]) {
+
+
+if (argc != 2) {
+    perror("Usage: perror. wrong number of arguments");
+    return -1;
+}
+
+int test = atoi(argv[1]);
+}
