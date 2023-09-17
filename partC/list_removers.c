5e6d4fc0dbab5423a4028a8093f760db00cf23a6
phong <wdz468@usask.ca>
2023-09-17 09:57:08 -0600

    Fixed initial errors for list_removers.c

--- partC/list_removers.c
+++ partC/list_removers.c
@@ -6,31 +6,38 @@ void *ListRemove(LIST *curList) {
     /*
     * If the current item at head of list
     */
-    if (*(curList->*currentItem)->*prevNode == NULL) {
-        NODE *oldHead = *(curList->*headPointer);
+    struct NODE *item1 = curList->currentItem;
+    if (item1->prevNode == NULL) {
+        struct NODE *oldHead = curList->headPointer;
         ListNext(curList);
-        oldHead->*nextNode = NULL;
-        *(curList->currentItem)->*prevNode = NULL;
-        curList->*headPointer = curList->*currentItem;
+        oldHead->nextNode = NULL;
+        struct NODE *itemMoved = curList->currentItem;
+        itemMoved->prevNode = NULL;
+        curList->headPointer = curList->currentItem;
         curList->totalItem -= 1;
+        return oldHead;
     }
     /*
     * If the current item at tail
     */
-    if (*(curList->currentItem)->*newNode == NULL) {
-        NODE *oldTail = curList->*tailPointer;
+    if (item1->nextNode == NULL) {
+        struct NODE *oldTail = curList->tailPointer;
         ListPrev(curList);
-        oldTail->*prevNode = NULL;
-        *(curList->currentItem)->*nextNode = NULL;
-        curList->*tailPointer = curList->*currentItem;
+        oldTail->prevNode = NULL;
+        struct NODE *itemMoved = curList->currentItem;
+        itemMoved->nextNode = NULL;
+        curList->tailPointer = curList->currentItem;
         curList->totalItem -= 1;
+        return oldTail;
     }
     
-    NODE *rmItem = *(curList->*currentItem);
-    ListNext(curList);
-    *(curList->*currentItem)->*prevNode = rmItem->*prevNode;
-    *(rmItem->*prevNode)->*nextNode = *(curList->*currentItem); 
-    rmItem->*prevNode = NULL;
-    rmItem->*nextNode = NULL;
+    struct NODE *rmItem = curList->currentItem;
+    struct NODE *nextItem = ListNext(curList);
+    nextItem->prevNode = rmItem->prevNode;
+    struct NODE *rmPrev = rmItem->prevNode;
+    rmPrev->nextNode = curList->currentItem; 
+    rmItem->prevNode = NULL;
+    rmItem->nextNode = NULL;
     curList->totalItem -= 1;
+    return rmItem;
 }
