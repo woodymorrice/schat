c333afbf90b13490daa4cebe476dfab8505e2daa
Woody Morrice <wam553@usask.ca>
2023-09-29 20:52:16 -0600

    partA2 tested, runs properly

--- partA2.c
+++ partA2.c
@@ -24,9 +24,9 @@ int mainp(int argc, char* argv[]) {
         args[2] = atoi(argv[3]);
     }
 
-    if (args[0] < 1 ||
-        args[1] < 1 ||
-        args[2] < 1) {
+    if (args[0] < 1 || 64    < args[0] ||
+        args[1] < 1 || 300   < args[1] ||
+        args[2] < 1 || 20000 < args[2]) {
         return EXIT_FAILURE;
     }
 
@@ -47,6 +47,8 @@ PROCESS parentThread(void *argPtr) {
 
     printf("parent args: %d, %d, %d\n", args[0], args[1], args[2]);
     for (i = 0; i < args[0]; i++) {
+        thrArr[i].beginTime = 0;
+        thrArr[i].sqCalls = 0;
         printf("index at creation: %d\n", i);
         if (PNUL == (int)(thrArr[i].entryId = 
                     Create((void(*)())childThread, 1048576,
@@ -57,7 +59,21 @@ PROCESS parentThread(void *argPtr) {
             printf("child pid at creation: %d\n", (int)thrArr[i].entryId);
         }
     }
-    
+
+    Sleep(100*args[1]);
+   
+    for (i = 0; i < args[0]; i++) {
+        if (PExists(thrArr[i].entryId)) {
+        Kill(thrArr[i].entryId);
+        printf("childThread %ld killed by parent."
+               " %d square calls, %ld ms\n",
+               thrArr[i].entryId,
+               thrArr[i].sqCalls,
+               10*((unsigned)Time() -
+                   thrArr[i].beginTime));
+        }
+    }
+
     Pexit();
 }
 
@@ -78,14 +94,18 @@ PROCESS childThread(void *argPtr) {
     for (index = 0; thrArr[index].entryId != id &&
             index < args[0]; index++);
 
+    thrArr[index].beginTime = (unsigned)Time();
+
     printf("index in child: %d\n", index);
 
     for (i = 1; i <= args[2]; i++) {
         square(i);
     }
 
-    printf("square calls in child %ld: %d\n", id,
-            thrArr[index].sqCalls);
+    printf("childThread %ld finished:"
+           "%d square calls, %ld ms\n",
+            id, thrArr[index].sqCalls,
+            10*((unsigned)Time() - thrArr[index].beginTime));
 
     Pexit();
 }
