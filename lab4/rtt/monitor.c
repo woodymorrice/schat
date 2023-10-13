7bb11c9ce840a9ce1b5bc6a6cba749565919bfb1
Woody Morrice <wam553@usask.ca>
2023-10-13 15:23:28 -0600

    monitor.c working properly

--- lab4/rtt/monitor.c
+++ lab4/rtt/monitor.c
@@ -9,89 +9,64 @@
 
 #include <list.h>
 
-LIST *conQueue[10];
-
 LIST *enterq;
 LIST *urgentq;
+LIST *conQueue[10];
+
+const int STKSIZE = 65536;
+const int ENTER = 20;
+const int LEAVE = 21;
+/* const int WAIT = 22;
+const int SIGNAL = 23; */
+const int SREPLY = 24;
 
 static RttThreadId server;
-const int STKSIZE = 65536;
-
-const int ENTER = 0;
-const int LEAVE = 1;
-const int WAIT = 2;
-const int SIGNAL = 3;
-const int SREPLY = 4;
-
 static unsigned int size = 4;
 static int reply;
 static int conds;
-static int currentCV;
+/* static int currentCV; */
 
 /* RttMonServer -- server PROCESS that handles
  * the coordination by putting processes on lists
  * according to the semantics of Monitors */
 RTTTHREAD MonServer() {
     int occupied;
-    RttThreadId sender;
+    /* RttThreadId sender; */
     int messageType;
     RttThreadId *waiting;
 
+    RttThreadId sendArr[100];
+    int index;
+
+    index = 0;
     occupied = 0;
 
     for (;;) {
         if (0 !=
-        RttReceive(&sender, &messageType, &size)) {
+        RttReceive(&sendArr[index], &messageType, &size)) {
             fprintf(stderr, "Error receiving message\n");
         }
 
         switch (messageType) {
-            case 0: /* ENTER */
-                printf("ENTER received\n");
-
-                /* if monitor is occupied add to entering queue */
-                if (occupied) {
-                    ListPrepend(enterq, &sender);
-                    printf("%d items in enterq\n", ListCount(enterq));
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9: /* WAIT */
+                /* printf("WAIT received\n"); */
+                if (messageType > conds - 1) {
+                    fprintf(stderr, "Error: CV does not exist\n");
                 }
-                /* else set monitor to occupied and reply to sender */
-                else {
-                    occupied = 1;
-                    RttReply(sender, (void*)&SREPLY,
-                             size);
-                }
-                break;
-
-            case 1: /* LEAVE */
-                printf("LEAVE received\n");
-
-                /* if urgentq not empty, take item off urgentq, reply */
-                if (ListCount(urgentq) > 0) {
-                    waiting = (RttThreadId*)ListTrim(urgentq);
-                    RttReply(*waiting, (void*)&SREPLY,
-                             size);
-                }
-                /* else if enterq not empty, take item off enterq, reply */
-                else if (ListCount(enterq) > 0) {
-                    waiting = (RttThreadId*)ListTrim(enterq);
-                    RttReply(*waiting, (void*)&SREPLY,
-                             size);
-                }
-                /* else monBusy to false, reply to thread executing leave */
-                else {
-                    occupied = 0;
-                    RttReply(sender, (void*)&SREPLY,
-                             size);
-                }
-                break;
-
-            case 2: /* WAIT */
-                printf("WAIT received\n");
 
                 /* add to cv_waitinglist */
-                ListPrepend(conQueue[currentCV], &sender);
-                printf("%d items in conQueue[%d]\n",
-                        ListCount(conQueue[currentCV]), currentCV);
+                ListPrepend(conQueue[messageType], &sendArr[index]);
+                /* printf("%d items in conQueue[%d]\n",
+                        ListCount(conQueue[messageType]), messageType); */
 
                 /* if urgentq not empty, take item off urgentq, reply */
                 if (ListCount(urgentq) > 0) {
@@ -111,27 +86,88 @@ RTTTHREAD MonServer() {
                 }
                 break;
 
-            case 3: /* SIGNAL */
-                printf("SIGNAL received\n");
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+                /* printf("SIGNAL received\n"); */
+                messageType -= 10;
+                if (messageType > conds - 1) {
+                    fprintf(stderr, "Error: CV does not exist\n");
+                }
 
                 /* if there is non-empty cvlist */
-                if (ListCount(conQueue[currentCV]) > 0) {
+                if (ListCount(conQueue[messageType]) > 0) {
                     /* take first item off the CV_waitinglist, reply */
                     waiting = 
-                        (RttThreadId*)ListTrim(conQueue[currentCV]);
+                        (RttThreadId*)ListTrim(conQueue[messageType]);
                     RttReply(*waiting, (void*)&SREPLY,
                              size);
                     /* add current thread to the urgentq */
-                    ListPrepend(urgentq, &sender);
-                    printf("%d items in urgentq\n", ListCount(urgentq));
+                    ListPrepend(urgentq, &sendArr[index]);
+                    /* printf("%d items in urgentq\n", ListCount(urgentq)); */
+                } 
+                /* else reply to signaller */
+                else {
+                    RttReply(sendArr[index], (void*)&SREPLY,
+                             size);
                 }
                 break;
 
-            default:
-                /* else reply to signaller */
-                RttReply(sender, (void*)&SREPLY,
-                         size);
+            case 20: /* ENTER */
+                /* printf("ENTER received\n"); */
+
+                /* if monitor is occupied add to entering queue */
+                if (occupied) {
+                    ListPrepend(enterq, &sendArr[index]);
+                    printf("%d items in enterq\n", ListCount(enterq));
+                }
+                /* else set monitor to occupied and reply to sender */
+                else {
+                    occupied = 1;
+                    RttReply(sendArr[index], (void*)&SREPLY,
+                             size);
+                }
                 break;
+
+            case 21: /* LEAVE */
+                /* printf("LEAVE received\n"); */
+                RttReply(sendArr[index], (void*)&SREPLY,
+                         size);
+
+                /* if urgentq not empty, take item off urgentq, reply */
+                if (ListCount(urgentq) > 0) {
+                    waiting = (RttThreadId*)ListTrim(urgentq);
+                    RttReply(*waiting, (void*)&SREPLY,
+                             size);
+                }
+                /* else if enterq not empty, take item off enterq, reply */
+                else if (ListCount(enterq) > 0) {
+                    waiting = (RttThreadId*)ListTrim(enterq);
+                    RttReply(*waiting, (void*)&SREPLY,
+                             size);
+                }
+                /* else monBusy to false, reply to thread executing leave */
+                else {
+                    occupied = 0;
+                }    
+                break;
+
+            default:
+                /* no other messages should be sent */
+                break;
+        }
+        if (index == 99) {
+            index = 0;
+        }
+        else {
+            index++;
         }
     }
 }
@@ -182,17 +218,16 @@ int RttMonLeave() {
 /* RttMonWait -- causes the calling process to
  * wait on some condition variable (CV) */
 int RttMonWait(int CV) {
-    currentCV = CV;
-    RttSend(server, (void*)&WAIT, size, 
+    RttSend(server, (void*)&CV, size, 
             &reply, &size);    
     return 0;
 }
 
 /* RttMonSignal -- signals the process at
  * the head of the CV queue to resume */
-int RttMonSignal(int CV) { 
-    currentCV = CV;
-    RttSend(server, (void*)&SIGNAL, size,
+int RttMonSignal(int CV) {
+    CV += 10;
+    RttSend(server, (void*)&CV, size,
             &reply, &size);
     return 0;
 }
