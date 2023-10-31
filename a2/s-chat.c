0459aa7c1b742e15e26c3cf97cbfcc635c514fd5
Woody Morrice <wam553@usask.ca>
2023-10-30 18:57:12 -0600

    s-chat finished

--- a2/s-chat.c
+++ a2/s-chat.c
@@ -27,18 +27,21 @@
 #include <list.h>
 
 #define BUF_SIZE 256
+#define HSTNM_SIZE 32
+#define MIN_ARGS 4
+#define MAX_ARGS 6
+#define MIN_PORT 30001
+#define MAX_PORT 40000
+#define STACK_SIZE 65536
+#define LIST_SIZE 50
 #define NFDS 1
 #define TIMEOUT 0
 #define MAX_DEST 2
 
 struct sDgram {
-    char message[256]; /* same as BUF_SIZE */
+    char message[BUF_SIZE];
     uint32_t s;
     uint32_t ms;
-    /*int32_t s_hi;
-    int32_t s_lo;
-    int32_t ms_hi;
-    int32_t ms_lo;*/
 };
 
 /* const ints so they can be pointed to */
@@ -60,7 +63,7 @@ static PID sSendDataPID;
 static PID sGetDataPID;
 static PID sDisplayDataPID;
 
-static char hostName[32];
+static char hostName[HSTNM_SIZE];
 static unsigned short int hostPort;
 static char* destName[MAX_DEST];
 static unsigned short int destPort[MAX_DEST];
@@ -72,7 +75,7 @@ int mainp(int argc, char* argv[]) {
     int j;
 
     /* get ports and names */
-    if (argc < 4 || argc > 12 || argc % 2 != 0) {
+    if (argc < MIN_ARGS || argc > MAX_ARGS || argc % 2 != 0) {
         printf("s-chat usage: s-chat localport " 
                "destname destport dest2name dest2port...\n");
         exit(-1);
@@ -80,9 +83,10 @@ int mainp(int argc, char* argv[]) {
     else {
         destinations = (argc - 2) / 2;
         hostPort = (unsigned short int)atoi(argv[1]);
-        if (hostPort < 30001 || hostPort > 40000) {
+        if (hostPort < MIN_PORT || hostPort > MAX_PORT) {
             fprintf(stderr, "User error: invalid port number, "
-                    "must be within the range 30001-40000\n");
+                    "must be within the range %d-%d\n",
+                    MIN_PORT, MAX_PORT);
             exit(-1);
         }
         j = 0;
@@ -90,9 +94,10 @@ int mainp(int argc, char* argv[]) {
             destName[j] = argv[i];
             i++;
             destPort[j] = (unsigned short int)atoi(argv[i]);
-            if (destPort[j] < 30001 || destPort[j] > 40000) {
+            if (destPort[j] < MIN_PORT || destPort[j] > MAX_PORT) {
                 fprintf(stderr, "User error: invalid port number, "
-                        "must be within the range 30001-40000\n");
+                        "must be within the range %d-%d\n",
+                        MIN_PORT, MAX_PORT);
                 exit(-1);
             }
             j++;
@@ -100,23 +105,23 @@ int mainp(int argc, char* argv[]) {
     }
 
     /* make threads */
-    if (PNUL == (sServerPID = Create(sServer, 65536,
+    if (PNUL == (sServerPID = Create(sServer, STACK_SIZE,
        "sServer", NULL, HIGH, USR))) {
         printf("Error creating sServer thread\n");
     }
-    if (PNUL == (sGetInputPID = Create(sGetInput, 65536,
+    if (PNUL == (sGetInputPID = Create(sGetInput, STACK_SIZE,
        "sGetInput", NULL, NORM, USR))) {
         printf("Error creating sGetInput thread\n");
     }
-    if (PNUL == (sSendDataPID = Create(sSendData, 65536,
+    if (PNUL == (sSendDataPID = Create(sSendData, STACK_SIZE,
        "sSendData", NULL, NORM, USR))) {
         printf("Error creating sSendData thread\n");
     }
-    if (PNUL == (sGetDataPID = Create(sGetData, 65536,
+    if (PNUL == (sGetDataPID = Create(sGetData, STACK_SIZE,
        "sGetData", NULL, NORM, USR))) {
         printf("Error creating sGetData thread\n"); 
     }
-    if (PNUL == (sDisplayDataPID = Create(sDisplayData, 65536,
+    if (PNUL == (sDisplayDataPID = Create(sDisplayData, STACK_SIZE,
        "sDisplayData", NULL, NORM, USR))) {
         printf("Error creating sDisplayData thread\n");
     }
@@ -178,7 +183,6 @@ void sServer() {
     void *received, *dReply;
     char *message, *reply;
 
-    /* reply = "goodbye"; */
 
     outgoing = ListCreate();
     sendWait = false;
@@ -194,7 +198,7 @@ void sServer() {
         /* Sending messages */
         if (sender == sGetInputPID) {
             message = (char*)received;
-            if (ListCount(outgoing) < 50) {
+            if (ListCount(outgoing) < LIST_SIZE) {
                 ListPrepend(outgoing, message);
             }
                 
@@ -232,7 +236,7 @@ void sServer() {
         /* Receiving messages */
         else
         if (sender == sGetDataPID) {
-            if (ListCount(incoming) < 50) {
+            if (ListCount(incoming) < LIST_SIZE) {
                 ListPrepend(incoming, received);
             }
             
@@ -280,15 +284,11 @@ void sServer() {
  * sends them to remote UNIX processes using UDP protocol */
 void sSendData() {
     struct hostent *result;
-    char buffer[256];
-    int bufflen;
+    int i;
+    char buffer[BUF_SIZE];
     int h_errnop;
 
     struct in_addr **addrs;
-    int i;
-    int j;
-    char ip_add[INET_ADDRSTRLEN];
-
     struct sockaddr_in *destPc[MAX_DEST];
 
     int msgLength;
@@ -296,24 +296,17 @@ void sSendData() {
     char* message;
     struct sDgram* dataPkg;
     
-    struct pollfd *canSend[1];
+    struct pollfd *canSend;
     int npoll;
     
     result = malloc(sizeof(struct hostent));
 
     for (i = 0; i < destinations; i++) {
-
-        /* printf("'%s' %u\n", destName[i], destPort[i]); */
-    
-        /*result = malloc(sizeof(struct hostent));*/
-
-        /*memset(buffer, '\0', 256);*/
     
         /* getting the network host entry information */
-        bufflen = 256;
         h_errnop = 0;
         if (0 != gethostbyname_r(destName[i], result,
-                                 buffer, bufflen,
+                                 buffer, BUF_SIZE,
                                  &result, &h_errnop)) {
             fprintf(stderr, "error: couldnt get network host entry\n");
             exit(-1);
@@ -325,13 +318,7 @@ void sSendData() {
             exit(-1);
         }
     
-        /* prints the list of IP addresses for debugging */
-        /* memset(ip_add, 0, INET_ADDRSTRLEN); */
         addrs = (struct in_addr **)result->h_addr_list;
-        /* for (j = 0; addrs[j] != NULL; j++) {
-            inet_ntop(AF_INET, &addrs[j], ip_add, INET_ADDRSTRLEN);
-            printf("'%s'\n", ip_add);
-        } */
 
         /* preparing my artisan sockaddr_in struct */
         destPc[i] = malloc(sizeof(struct sockaddr_in));
@@ -339,15 +326,13 @@ void sSendData() {
         destPc[i]->sin_family = AF_INET;
         destPc[i]->sin_port = htons(destPort[i]);
         destPc[i]->sin_addr = *addrs[0];
-
-        /*free(result);*/
     }
-    
     free(result);
-    canSend[0] = malloc(sizeof(struct pollfd));
 
-    canSend[0]->fd = sockfd;
-    canSend[0]->events = POLLOUT;
+    canSend = malloc(sizeof(struct pollfd));
+
+    canSend->fd = sockfd;
+    canSend->events = POLLOUT;
 
     for (;;) {
         reply = Send(sServerPID, (void*)&STD_MSG, &msgLength);
@@ -359,7 +344,7 @@ void sSendData() {
             message = (char*)reply;
             dataPkg = packDgram(message);
 
-            npoll = poll(canSend[0], NFDS, TIMEOUT);
+            npoll = poll(canSend, NFDS, TIMEOUT);
             if (npoll == -1) {
                 fprintf(stderr, "error %d: send poll failed\n", errno);
                 exit(-1);
@@ -391,7 +376,7 @@ void sGetData() {
 
     struct sDgram* dataPkg;
 
-    struct pollfd *msgWaiting[1];
+    struct pollfd *msgWaiting;
     int npoll;
 
     int bytes;
@@ -400,16 +385,16 @@ void sGetData() {
     unsigned int fromlen;
 
 
-    msgWaiting[0] = malloc(sizeof(struct pollfd));
+    msgWaiting = malloc(sizeof(struct pollfd));
 
-    msgWaiting[0]->fd = sockfd;
-    msgWaiting[0]->events = POLLIN;
+    msgWaiting->fd = sockfd;
+    msgWaiting->events = POLLIN;
 
     from = malloc(sizeof(struct sockaddr_in));
     fromlen = sizeof(struct sockaddr_in);
 
     for (;;) {
-        npoll = poll(msgWaiting[0], NFDS, TIMEOUT);
+        npoll = poll(msgWaiting, NFDS, TIMEOUT);
         if (npoll == -1) {
             fprintf(stderr, "error %d: recv poll failed\n", errno);
             exit(-1);
@@ -458,16 +443,11 @@ void sDisplayData() {
 
 /* prepSocket -- prepares and returns local network socket */
 int prepSocket() {
-    /*static char hostname[32];*/ /* for getting the hostname */
-
     struct hostent *result; /* for grabbing the network host entry */
-    char buffer[256];
-    int  bufflen;
+    char buffer[BUF_SIZE];
     int  h_errnop;
 
-    struct in_addr **addrs; /* to print the IP for debugging */
-    int i;
-    char ip_add[INET_ADDRSTRLEN];
+    struct in_addr **addrs;
 
     struct sockaddr_in *hostPc; /* handcrafted */
     int sock; /* socket file descriptor */
@@ -477,25 +457,17 @@ int prepSocket() {
         fprintf(stderr, "error %d: could not get host name\n", errno);
         exit(-1);
     }
-    /* printf("'%s' %u\n", hostName, hostPort); */
 
     /* getting the network host entry information */
     result = malloc(sizeof(struct hostent));
-    bufflen = 256;
     h_errnop = 0;
-    if (0 != gethostbyname_r(&hostName[0], result, &buffer[0], bufflen,
+    if (0 != gethostbyname_r(&hostName[0], result, &buffer[0], BUF_SIZE,
                 &result, &h_errnop)) {
         fprintf(stderr, "error: could not get network host entry\n");
         exit(-1);
     }
     
-    /* prints the list of IP addresses for debugging */
-    /* memset(ip_add, 0, INET_ADDRSTRLEN); */
     addrs = (struct in_addr **)result->h_addr_list;
-    /* for (i = 0; addrs[i] != NULL; i++) {
-        inet_ntop(AF_INET, &addrs[i], ip_add, INET_ADDRSTRLEN);
-        printf("'%s'\n", ip_add);
-    } */
 
     /* preparing my artisan sockaddr_in struct */
     hostPc = malloc(sizeof(struct sockaddr_in));
@@ -533,11 +505,6 @@ struct sDgram* packDgram(char* msg) {
     struct sDgram* dgram;
     struct timeval curTime;
 
-    /*int32_t s_hi;
-    int32_t s_lo;
-    int32_t ms_hi;
-    int32_t ms_lo;*/
-
     gettimeofday(&curTime, NULL);
 
     dgram = malloc(sizeof(struct sDgram));
@@ -545,19 +512,7 @@ struct sDgram* packDgram(char* msg) {
     memcpy(dgram->message, msg, strlen(msg)+1); 
     dgram->message[strlen(msg)] = '\0';
 
-    /*dgram->secs = (int64_t)htonl(curTime.tv_sec);*/
-    /*dgram->ms = (int64_t)htonl(curTime.tv_usec);*/
-
-    /*s_hi = (int32_t)(curTime.tv_sec  >> 32);
-    s_lo = (int32_t)(curTime.tv_sec & 0xffffffff);
-    ms_hi = (int32_t)((int64_t)curTime.tv_usec >> 32);
-    ms_lo = (int32_t)(curTime.tv_usec & 0xffffffff);*/
-
     dgram->s = htonl(curTime.tv_sec);
-    /*dgram->s_hi = htonl(s_hi);
-    dgram->s_lo = htonl(s_lo);
-    dgram->ms_hi = htonl(ms_hi);
-    dgram->ms_lo = htonl(ms_lo);*/
     dgram->ms = htonl((uint32_t)curTime.tv_usec);
 
     return dgram;
@@ -566,17 +521,12 @@ struct sDgram* packDgram(char* msg) {
 /* printTime -- prints the date in a nice format */
 void printTime(struct sDgram* time) {
     long int s;
-
-    /*time_t secs;*/
     uint32_t ms;
     char str[64];
     struct tm timeptr;
 
     s = (signed long)ntohl(time->s);
     ms = ntohl(time->ms);
-    /*ms = ntohll(time->ms);*/
-    /*s = (((int64_t)ntohl(time->s_hi)) << 32) + ntohl(time->s_lo);*/
-    /*ms = (((int64_t)ntohl(time->ms_hi)) << 32) | ntohl(time->ms_lo);*/
 
     timeptr = *localtime(&s);
     strftime(str, sizeof(str), "%x %X", &timeptr);
