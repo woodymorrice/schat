bfc4b61824fc3225df1af41ddf45c7cc21760e75
Woody Morrice <wam553@usask.ca>
2023-11-22 23:47:57 -0600

    a3 - simple and three tests pass

--- xv6-new/user/cat.d
+++
@@ -1,2 +0,0 @@
-user/cat.o: user/cat.c kernel/types.h kernel/stat.h kernel/fcntl.h \
- user/user.h
--- xv6-new/user/uthread.d
+++
@@ -1,3 +0,0 @@
-user/uthread.o: user/uthread.c kernel/types.h kernel/stat.h user/user.h \
- user/uthread.h \
- /student/cmpt332/xv6-2023/riscv/lib/gcc/riscv64-unknown-elf/12.2.0/include/stddef.h
